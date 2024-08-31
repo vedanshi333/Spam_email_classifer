32aea83eb79f6412e89c0267acb6bf809017e95a
vedanshi333 <106459899+vedanshi333@users.noreply.github.com>
2024-08-31 14:09:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import streamlit as st
 @st.cache
 def load_data():
     df = pd.read_csv('spam.csv')
-    X = df['text']
+    X = df['message']
     y = df['label']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     return X_train, X_test, y_train, y_test
